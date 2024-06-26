3eab82d58368149bcd8ddc1b06f6cae19cde4f26
gocrushgoals <gocrushgoals@gmail.com>
2024-06-26 09:12:45 -0400

    rebuild dist

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,28 +3,22 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
   event.preventDefault();
-  // stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  //remove the hidden class from the install button
-  butInstall.classList.remove('hidden', false);
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+  // Update UI notify the user they can install the PWA
+  butInstall.removeAttribute('hidden');
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  const promptEvent = window.deferredPrompt;
-  
-    if (!promptEvent) {
-    return;
-    }
-    // Show the prompt
-    promptEvent.prompt();
-    // Wait for the user to respond to the prompt
-    const result = await promptEvent.userChoice;
-    // Log the result
-    console.log(result.outcome);
-    // Clear the saved event
-    window.deferredPrompt = null;
+
+  deferredPrompt.prompt(); 
+  const { outcome } = await deferredPrompt.userChoice;  
+  console.log(`User response to the install prompt: ${outcome}`);  
+  butInstall.setAttribute('hidden', '');  
+  deferredPrompt = null;
 });
 
 // TODO: Add an handler for the `appinstalled` event
